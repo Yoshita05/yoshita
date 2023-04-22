8408079a06aa23cc92f63ad7b73a3b1870fcbda3
Yoshita05 <131455346+Yoshita05@users.noreply.github.com>
2023-04-22 10:17:37 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-let year = document.getElementById("year");
-year.innerHTML = new Date().getFullYear();
-// Reference from Friend for up arrow
-let scrollToTopBtn = document.getElementsByClassName("up-arrow")[0];
-
-scrollToTopBtn.addEventListener("click", () => {
-    document.documentElement.scrollTop = 0;
-})
